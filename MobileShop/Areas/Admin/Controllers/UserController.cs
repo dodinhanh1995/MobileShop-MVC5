858a44e0fec39b150e295e2a20d0b28829ac479e
anh do <dodinhanh195@gmail.com>
2017-07-17 10:36:50 +0700

    Thêm create, update, getdetail UserDAO

--- MobileShop/Areas/Admin/Controllers/UserController.cs
+++ MobileShop/Areas/Admin/Controllers/UserController.cs
@@ -10,5 +10,22 @@ namespace MobileShop.Areas.Admin.Controllers
         {
             return View(UserDAO.Instance.GetListAll());
         }
+
+        [HttpPost]
+        public ActionResult Delete(FormCollection form)
+        {
+            string[] userIds = form["userId"].Split(',');
+
+            foreach (string item in userIds)
+            {
+                UserDAO.Instance.Delete(item);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
--- Model/DAO/UserDAO.cs
+++ Model/DAO/UserDAO.cs
@@ -1,6 +1,7 @@
 ﻿using Model.EF;
 using System.Linq;
 using System.Collections.Generic;
+using System.Data.Entity;
 
 namespace Model.DAO
 {
@@ -33,6 +34,56 @@ namespace Model.DAO
         public List<User> GetListAll()
         {
             return db.Users.ToList();
+        }
+
+        public bool Create(User user)
+        {
+            try
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public User GetDetail(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public bool Update(User user)
+        {
+            try
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(string id)
+        {
+            try
+            {
+                var user = db.Users.Find(int.Parse(id));
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
