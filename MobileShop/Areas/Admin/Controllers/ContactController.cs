daa4450a7c035a46ab82dd0fa176f863c1f47515
anh do <dodinhanh195@gmail.com>
2017-07-20 09:51:11 +0700

    Hoàn thành quản lý liên hệ

---
+++ MobileShop/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,55 @@
+﻿using Model.DAO;
+using Model.EF;
+using System.Net;
+using System.Web.Mvc;
+
+namespace MobileShop.Areas.Admin.Controllers
+{
+    public class ContactController : BaseController
+    {
+        // GET: Admin/Contact
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.StatusSortParm = string.IsNullOrEmpty(sortOrder) ? "status_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.CurrentFilter = searchString;
+
+            return View(ContactDAO.Instance.GetListAll(sortOrder, searchString, page));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(FormCollection form)
+        {
+            string[] contactIds = form["contactId"].Split(',');
+
+            foreach (string item in contactIds)
+            {
+                ContactDAO.Instance.Delete(item);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Contact contact = ContactDAO.Instance.GetDetail(id.Value);
+            if (contact == null)
+                return HttpNotFound();
+            return View(contact);
+        }
+
+        public bool ChangeStatus(int id)
+        {
+            return ContactDAO.Instance.ChangeStatus(id);
+        }
+
+    }
+}
---
+++ MobileShop/Assets/Admin/controller/contact.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('.btnStatus').off('click').on('click', function (e) {
+        e.preventDefault();
+        var btn = $(this);
+        $.get('/Contact/ChangeStatus', {id : $(btn).data('id')}, function (status) {
+            if (status == "True")
+                $(btn).text('Đã đọc').removeClass('text-danger').addClass('text-success');
+            else
+                $(btn).text('Chưa đọc').removeClass('text-success').addClass('text-danger');
+        });
+    });
+})
+
+
