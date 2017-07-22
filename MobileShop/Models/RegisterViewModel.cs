b6d58f1a8eff85caa6ea96da3a6454629d69fa15
anh do <dodinhanh195@gmail.com>
2017-07-22 20:16:53 +0700

    Update Models Folder

---
+++ MobileShop/Models/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileShop.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="Vui lòng nhập tên đăng nhập!")]
+        [Display(Name = "Tên đăng nhập")]
+        public string UserName { get; set; }
+
+        
+
+        [Required(ErrorMessage ="Vui lòng nhập mật khẩu!")]
+        [StringLength(32, ErrorMessage = "Mật khẩu tối thiểu phải 6 ký tự", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Xác nhận mật khẩu")]
+        [Compare("Password", ErrorMessage = "Xác nhận mật khẩu không đúng!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ email!")]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không hợp lệ!")]
+        [Display(Name = "Địa chỉ email")]
+        public string Email { get; set; }
+
+    }
+}
